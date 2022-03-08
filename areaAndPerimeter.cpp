f1384de73b86aa6c2a8d704708e49d5abf1b84c1
Miguel Santacruz <miguel.santacruz@mths.ca>
2022-03-08 01:46:12 +0000

    Area and perimeter

---
+++ areaAndPerimeter.cpp
@@ -0,0 +1,34 @@
+// Copyright (c) 2022 Miguel Santacruz All rights reserved
+//
+// Created by: Miguel Santacruz
+// Created on: Mar 2022
+// This program calculates area and perimeter of a rectangle
+//    with length and width imputed by the user
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    // This function calculates area and perimeter
+    int length;
+    int width;
+    int areaOfRectangle;
+    int perimeterOfRectangle;
+
+    // Input
+    std::cout << "Enter length of rectangle (cm): " << std::endl;
+    std::cin >> length;
+    std::cout << "Enter width of rectangle (cm): " << std::endl;
+    std::cin >> width;
+
+    // Process
+    areaOfRectangle = length * width;
+    perimeterOfRectangle = 2 * (length + width);
+
+    // Output
+
+    std::cout << "The area of the rectangle is " << areaOfRectangle << " cm²." << std::endl;
+    std::cout << "The perimeter of the rectangle is " << perimeterOfRectangle << " cm." << std::endl;
+
+    std::cout << "\nDone." << std::endl;
+}
